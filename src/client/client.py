99df8d7070139e38afecc1faa1ed7c350ebed419
Aleksander Wardyn <aleksander.wardyn@jit.team>
2021-01-04 20:00:47 +0100

    Add implementation of adding client

--- src/client/client.py
+++ src/client/client.py
@@ -6,7 +6,17 @@ class Client:
         self.api = 'https://virtual-shop.pl/api'
 
     def add_client(self, body):
-        pass
+        if isinstance(body, type({'something': 'somethingelse'})) is False:
+            raise TypeError('Body must be dictionary')
+        if 'email' not in body:
+            raise ValueError('Body must contain email, name and surname')
+        response = requests.post(self.api+'/add', json=body)
+        if 200 <= response.status_code <= 299:
+            return response
+        elif response.status_code == 409:
+            return 'User of given email exists'
+        else:
+            return 'Some horrible error happend'
 
     def get_client(self, client_id):
         pass
