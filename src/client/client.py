d0347deb094212d5a6d0154699a9bf81bfd1e604
Aleksander Wardyn <aleksander.wardyn@jit.team>
2021-01-04 12:50:08 +0100

    Prepare client class for testing, add requirements.txt and status badge

---
+++ src/client/client.py
@@ -0,0 +1,18 @@
+import requests
+
+
+class Client:
+    def __init__(self):
+        self.api = 'https://virtual-shop.pl/api'
+
+    def add_client(self, body):
+        pass
+
+    def get_client(self, client_id):
+        pass
+
+    def get_clients(self):
+        pass
+
+    def update_client(self, client_id, body):
+        pass
